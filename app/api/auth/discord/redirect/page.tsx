e2369bebb48f82fa9237d5cdb095df62748b8eba
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-13 21:41:24 +0400

    feat: add oauth redirect page (does nothing yet)

---
+++ app/api/auth/discord/redirect/page.tsx
@@ -0,0 +1,8 @@
+export default function Redirect() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-24">
+      <p>You probably shouldn&apos;t be seeing this..</p>
+      <a className="underline" href="/">Go back</a>
+    </main>
+  )
+}
