26e68a710536c957b9d44c67670a200a042a1b94
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-14 02:12:26 +0400

    feat: add oauth2

---
+++ app/api/auth/discord/redirect/route.tsx
@@ -0,0 +1,66 @@
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+async function exchangeCode(code: string) {
+  const response = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      "client_id": process.env.CLIENT_ID as string,
+      "client_secret": process.env.CLIENT_SECRET as string,
+      "grant_type": "authorization_code",
+      "code": code,
+      "redirect_uri": process.env.REDIRECT_URI as string,
+    })
+  });
+
+  const data = await response.json();
+
+  return data;
+};
+
+async function refreshToken(refreshToken: RequestCookie) {
+  const response = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      "client_id": process.env.CLIENT_ID as string,
+      "client_secret": process.env.CLIENT_SECRET as string,
+      "grant_type": "refresh_token",
+      "refresh_token": refreshToken.value,
+    })
+  });
+
+  const data = await response.json();
+
+  return data;
+};
+
+export async function GET(request: NextRequest) {
+  const cookieStore = cookies();
+  const refreshTokenCookie = cookieStore.get("refresh_token");
+
+  const searchParams = request.nextUrl.searchParams;
+  const code = searchParams.get('code');
+
+  let data;
+
+  if (code) {
+    data = await exchangeCode(code);
+  } else if (refreshTokenCookie) {
+    data = await refreshToken(refreshTokenCookie);
+  } else {
+    redirect(process.env.OAUTH2_URI as string);
+  }
+
+  cookieStore.set("access_token", data.access_token, { secure: true });
+  cookieStore.set("refresh_token", data.refresh_token, { secure: true });
+
+  redirect("/dashboard");
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-24">
       <h1 className="text-2xl font-bold">Панель управления</h1>
-      <a className="p-2 border-2 border-current rounded" href="https://discord.com/oauth2/authorize?client_id=1119965914859843655&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify+guilds">
+      <a className="p-2 border-2 border-current rounded" href="/api/auth/discord/redirect">
         Войти через Discord
       </a>
     </main>
